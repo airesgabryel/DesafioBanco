c145166f90f6b6d634cc699d418340ffb4e2f4bf
airesgabryel <adriananascimentoalves6@gmail.com>
2025-11-19 03:04:33 -0300

    Implementando cálculos

--- src/Desafio.java
+++ src/Desafio.java
@@ -24,11 +24,30 @@ public class Desafio {
         while(opcao != 4){
             System.out.println(menu);
             opcao = leitura.nextInt();
+
+
+            if (opcao == 1){
+                System.out.println("O saldo atualizado é: " + saldo);
+            } else if (opcao == 2){
+                System.out.println("Valor da transferência: ");
+                double valor = leitura.nextDouble();
+                if (valor > saldo ){
+                    System.out.println("Saldo insuficiente!");
+                } else {
+                    saldo = saldo - valor;
+                    //saldo -= valor;
+                    System.out.println("Saldo atual: " + saldo );
+                }
+            } else if (opcao == 3){
+                System.out.println("Valor recebido: ");
+                double valor = leitura.nextDouble();
+                saldo += valor;
+                System.out.println("Saldo atual: " + saldo);
+            } else if (opcao != 4){
+                System.out.println("Opção inválida");
+            }
+
         }
-
-
-
-
 
     }
 }
